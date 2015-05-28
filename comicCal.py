377f17bec1d2201332909751b64d1c4a0ae3c371
Henry Lewis <hlewis@uchicago.edu>
2015-05-28 01:36:19 -0500

    small things

--- comicCal.py
+++ comicCal.py
@@ -4,7 +4,6 @@ from oauth2client.client import SignedJwtAssertionCredentials
 from httplib2 import Http
 from apiclient.discovery import build
 import json
-import pprint
 
 comics = ['the-wicked-the-divine','saga']
 
@@ -12,7 +11,7 @@ url = 'https://imagecomics.com/comics/release-archive/'
 headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36' }
 
 
-with open('creditionals.json') as data_file:    
+with open('credentials.json') as data_file:    
     data = json.load(data_file)
     client_email = data['client_email']
     private_key = bytes(data['private_key'], 'UTF-8')
