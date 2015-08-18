709ec8244ac8c1458c20981c5e4e481edec78b6f
Henry <henry@DeepThought.server>
2015-08-17 19:11:12 -0500

    exec

--- comicCal.py
+++ comicCal.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 from bs4 import BeautifulSoup
 import urllib.request
 from oauth2client.client import SignedJwtAssertionCredentials
@@ -11,7 +13,7 @@ url = 'https://imagecomics.com/comics/release-archive/'
 headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36' }
 
 
-with open('credentials.json') as data_file:    
+with open('/home/henry/server/jobs/comicCal/credentials.json') as data_file:    
     data = json.load(data_file)
     client_email = data['client_email']
     private_key = bytes(data['private_key'], 'UTF-8')
